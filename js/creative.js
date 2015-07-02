22b69b320c8d6143991a7a9da724e0af1d78f3b8
Tim Rücker <tim.ruecker@mcbring.de>
2015-07-02 10:46:46 +0200

    init

--- js/creative.js
+++ js/creative.js
@@ -7,20 +7,11 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
-    })
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -43,6 +34,6 @@
     })
 
     // Initialize WOW.js Scrolling Animations
-    new WOW().init();
+    //new WOW().init();
 
 })(jQuery); // End of use strict
